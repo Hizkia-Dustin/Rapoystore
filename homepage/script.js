c4c0bfd7f83e32d38afbd106863522e73a544a57
Hizkia-Dustin <168442382+Hizkia-Dustin@users.noreply.github.com>
2025-06-02 15:47:09 +0700

    Update script.js

--- homepage/script.js
+++ homepage/script.js
@@ -124,68 +124,35 @@ document.addEventListener('DOMContentLoaded', () => {
       price: 'Rp 350.000',
       rating: '5.0'
     },
-    {
-      imageSrc: '/homepage/img/crocs.img/pink crocs.png',
-      name: 'Crocs Classic Clog - Pink',
-      price: 'Rp 350.000',
-      rating: '4.7'
-    },
-    {
-      imageSrc: '/homepage/img/crocs.img/crocs lagoon.png',
-      name: 'Crocs Classic Clog - Green',
-      price: 'Rp 350.000',
-      rating: '4.6'
-    },
-    {
-      imageSrc: '/homepage/img/crocs.img/crocs tint.png',
-      name: 'Crocs Classic Clog - Light Blue',
-      price: 'Rp 350.000',
-      rating: '4.5'
-    }
+    
   ];
 
   const recommendedProducts = [
     {
-      imageSrc: 'https://images.unsplash.com/photo-1542291026-7eec264c678a?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-      discount: '10% OFF',
-      name: 'Crocs Unisex Classic Clog ',
-      price: 'Rp 350.000',
-      originalPrice: '$200',
+      imageSrc: '/homepage/img/sepatu.img/adidas whire.jpg',
+      name: 'Adidas Campus Off White',
+      price: 'Rp 1.000.000',
       rating: '4.5'
     },
     {
-      imageSrc: 'https://images.unsplash.com/photo-1511746315387-a2d88da9bbcd?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-      discount: '5% OFF',
-      name: 'Puma casual shoes',
-      price: '$114',
-      originalPrice: '$120',
+      imageSrc: '/homepage/img/sepatu.img/sepatu convers hitam.jpg',
+      name: 'Converse Chuck Tylor 70s High Black And White',
+      price: 'Rp 750.000',
       rating: '4.2'
     },
-     {
-      imageSrc: 'https://images.unsplash.com/photo-1595950653106-6ca6f4f690d9?ixlib=rb-1.2.1&auto=format&fit=crop&crop=w=750&q=80',
-      discount: '18% OFF',
-      name: 'Converse Chuck Taylor',
+    {
+      imageSrc: '/homepage/img/sepatu.img/nike biru.jpg',
+      name: 'Nike Blue',
       price: '$74',
-      originalPrice: '$90',
       rating: '4.7'
     },
     {
-      imageSrc: 'https://images.unsplash.com/photo-1505740420928-95ed7192a5e1?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-      discount: '22% OFF',
-      name: 'Headphones E505',
+      imageSrc: '/homepage/img/sepatu.img/nike emas.jpg',
+      name: 'Nike Gold',
       price: '$155',
-      originalPrice: '$199',
       rating: '4.9'
     },
-
-    {
-      imageSrc: 'https://images.unsplash.com/photo-1505740420928-95ed7192a5e1?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-      discount: '22% OFF',
-      name: 'Headphones E505',
-      price: '$155',
-      originalPrice: '$199',
-      rating: '4.9'
-    },
+   
   ];
 
   const clothingProducts = [
@@ -236,14 +203,14 @@ document.addEventListener('DOMContentLoaded', () => {
             <i class="fas fa-heart"></i>
           </span>
         </a>
-        <div class="mt-4 px-5 pb-5">
+        <div class="mt-4 px-5 pb-5 flex flex-col">
           <a href="#">
             <h5 class="text-xl tracking-tight text-slate-900">${product.name}</h5>
           </a>
           <div class="mt-2 mb-5 flex items-center justify-between">
             <p>
               <span class="text-3xl font-bold text-slate-900">${product.price}</span>
-            </p>
+            </p> 
             <div class="flex items-center">
               <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
@@ -251,7 +218,7 @@ document.addEventListener('DOMContentLoaded', () => {
               <span class="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">${product.rating}</span>
             </div>
           </div>
-          <a href="#" class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+          <a href="#" class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300 mt-auto">
             <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
             </svg>
